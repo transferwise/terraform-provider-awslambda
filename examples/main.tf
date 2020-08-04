63ddaa8d12e475f1ce5a0475a146bdd958a6dbe7
Dedunu Dhananjaya <dedunumax@users.noreply.github.com>
2020-08-04 13:52:47 +0300

    Create main.tf

---
+++ examples/main.tf
@@ -0,0 +1,20 @@
+provider "awslambda" {
+  region   = "us-west-1"
+  profile  = "default"
+  role_arn = "arn:aws:iam::account-id:role/role-name-with-path"
+}
+
+resource "aws_lambda_invocation" "example" {
+  function_name = "${aws_lambda_function.lambda_function_test.function_name}"
+
+  input = <<JSON
+ {
+   "key1": "value1",
+   "key2": "value2"
+ }
+ JSON
+}
+
+output "result_entry" {
+  value = jsondecode(aws_lambda_invocation.example.result)["key1"]
+}
