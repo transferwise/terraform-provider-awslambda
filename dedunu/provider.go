52844f14bcde3b6e2fc945e565bd060c9ab07a53
William Lord <william.lord@transferwise.com>
2021-10-08 17:18:11 +0100

    Update packages and synchronise with hashicorp/terraform-provider-aws

--- dedunu/provider.go
+++ dedunu/provider.go
@@ -2,10 +2,8 @@ package dedunu
 
 import (
 	"fmt"
-
 	"github.com/aws/aws-sdk-go/aws/session"
 	awsbase "github.com/hashicorp/aws-sdk-go-base"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/logging"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -19,25 +17,21 @@ func Provider() terraform.ResourceProvider {
 	provider := &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"profile": {
-				Type:     schema.TypeString,
-				Required: true,
-				Description: "The profile for API operations. If not set, the default profile\n" +
-					"created with `aws configure` will be used.",
-				InputDefault: "",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: descriptions["profile"],
 			},
+
 			"region": {
 				Type:     schema.TypeString,
 				Required: true,
-				Description: "The region where AWS operations will take place. Examples\n" +
-					"are us-east-1, us-west-2, etc.",
-				InputDefault: "us-west-1",
-			},
-			"role_arn": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Description: "Amazon Resource Name of an IAM Role to assume prior to making\n" +
-					"the AWS Lambda call.",
-				InputDefault: "",
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"AWS_REGION",
+					"AWS_DEFAULT_REGION",
+				}, nil),
+				Description:  descriptions["region"],
+				InputDefault: "us-east-1", // lintignore:AWSAT003
 			},
 		},
 
@@ -47,53 +41,35 @@ func Provider() terraform.ResourceProvider {
 	}
 
 	provider.ConfigureFunc = func(d *schema.ResourceData) (interface{}, error) {
-		terraformVersion := provider.TerraformVersion
-
-		awsbaseConfig := &awsbase.Config{
-			AccessKey:                   "",
-			AssumeRoleARN:               d.Get("role_arn").(string),
-			AssumeRoleDurationSeconds:   0,
-			AssumeRoleExternalID:        "",
-			AssumeRolePolicy:            "",
-			AssumeRolePolicyARNs:        []string{},
-			AssumeRoleSessionName:       "",
-			AssumeRoleTags:              map[string]string{},
-			AssumeRoleTransitiveTagKeys: []string{},
-			CallerDocumentationURL:      "https://registry.terraform.io/providers/hashicorp/aws",
-			CallerName:                  "Terraform AWS Provider",
-			CredsFilename:               "",
-			DebugLogging:                logging.IsDebugOrHigher(),
-			IamEndpoint:                 "",
-			Insecure:                    false,
-			MaxRetries:                  25,
-			Profile:                     d.Get("profile").(string),
-			Region:                      d.Get("region").(string),
-			SecretKey:                   "",
-			SkipCredsValidation:         false,
-			SkipMetadataApiCheck:        false,
-			SkipRequestingAccountId:     false,
-			StsEndpoint:                 "",
-			Token:                       "",
-			UserAgentProducts: []*awsbase.UserAgentProduct{
-				{Name: "APN", Version: "1.0"},
-				{Name: "HashiCorp", Version: "1.0"},
-				{Name: "Terraform", Version: terraformVersion,
-					Extra: []string{"+https://www.terraform.io"}},
-			},
-		}
-
-		sess, err := awsbase.GetSession(awsbaseConfig)
-
-		if err != nil {
-			return nil, fmt.Errorf("error configuring AWS Lamda provider: %w", err)
-		}
-
-		config := &providerConfig{
-			session: sess,
-		}
-
-		return config, nil
+		return providerConfigure(d)
 	}
 
 	return provider
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	sess, err := awsbase.GetSession(&awsbase.Config{
+		Profile: d.Get("profile").(string),
+		Region:  d.Get("region").(string),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error configuring Terraform AWS Provider: %w", err)
+	}
+
+	config := &providerConfig{
+		session: sess,
+	}
+	return config, nil
+}
+
+var descriptions map[string]string
+
+func init() {
+	descriptions = map[string]string{
+		"region": "The region where AWS operations will take place. Examples\n" +
+			"are us-east-1, us-west-2, etc.", // lintignore:AWSAT003
+
+		"profile": "The profile for API operations. If not set, the default profile\n" +
+			"created with `aws configure` will be used.",
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/dedunu/terraform-provider-awslambda
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.31.9
-	github.com/hashicorp/aws-sdk-go-base v0.5.0
-	github.com/hashicorp/terraform-plugin-sdk v1.15.0
-	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	github.com/aws/aws-sdk-go v1.40.56
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/hashicorp/aws-sdk-go-base v1.0.0
+	github.com/hashicorp/go-hclog v0.15.0 // indirect
+	github.com/hashicorp/go-plugin v1.4.1 // indirect
+	github.com/hashicorp/terraform-plugin-sdk v1.17.2
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/zclconf/go-cty v1.8.4 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20210502180810-71e4cd670f79 // indirect
 )
